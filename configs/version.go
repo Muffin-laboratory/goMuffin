9af72608db11e229a1c93edda1ba2f7c35637f39
Siwoo Jeon <me@migan.co.kr>
2025-04-13 11:25:17 +0900

    chore: Updating version

--- configs/version.go
+++ configs/version.go
@@ -7,7 +7,7 @@ import (
 	"git.wh64.net/muffin/goMuffin/utils"
 )
 
-const MUFFIN_VERSION = "5.0.0-gopher_preview.250408a"
+const MUFFIN_VERSION = "5.0.0-gopher_release.250413a"
 
 var updatedString string = utils.Decimals.FindAllStringSubmatch(MUFFIN_VERSION, -1)[3][0]
 
