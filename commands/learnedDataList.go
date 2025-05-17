24254f0b6dbd2664738109fd7b319cab7017872d
Siwoo Jeon <me@migan.co.kr>
2025-05-17 11:53:38 +0900

    fix: length

--- commands/learnedDataList.go
+++ commands/learnedDataList.go
@@ -140,7 +140,6 @@ func learnedDataListRun(s *discordgo.Session, m any, args *[]string) {
 		if match := utils.RegexpLearnQueryLength.FindStringSubmatch(query); match != nil {
 			var err error
 			length, err = strconv.Atoi(match[1])
-			fmt.Printf("err: %v\n", err)
 
 			if err != nil {
 				s.ChannelMessageSendEmbedReply(m.ChannelID, &discordgo.MessageEmbed{
@@ -150,24 +149,24 @@ func learnedDataListRun(s *discordgo.Session, m any, args *[]string) {
 				}, m.Reference())
 				return
 			}
-		}
 
-		if float64(length) < LIST_MIN_VALUE {
-			s.ChannelMessageSendEmbedReply(m.ChannelID, &discordgo.MessageEmbed{
-				Title:       "❌ 오류",
-				Description: fmt.Sprintf("개수의 값은 %d보다 커야해요.", int(LIST_MIN_VALUE)),
-				Color:       utils.EmbedFail,
-			}, m.Reference())
-			return
-		}
+			if float64(length) < LIST_MIN_VALUE {
+				s.ChannelMessageSendEmbedReply(m.ChannelID, &discordgo.MessageEmbed{
+					Title:       "❌ 오류",
+					Description: fmt.Sprintf("개수의 값은 %d보다 커야해요.", int(LIST_MIN_VALUE)),
+					Color:       utils.EmbedFail,
+				}, m.Reference())
+				return
+			}
 
-		if float64(length) > LIST_MAX_VALUE {
-			s.ChannelMessageSendEmbedReply(m.ChannelID, &discordgo.MessageEmbed{
-				Title:       "❌ 오류",
-				Description: fmt.Sprintf("개수의 값은 %d보다 작아야해요.", int(LIST_MAX_VALUE)),
-				Color:       utils.EmbedFail,
-			}, m.Reference())
-			return
+			if float64(length) > LIST_MAX_VALUE {
+				s.ChannelMessageSendEmbedReply(m.ChannelID, &discordgo.MessageEmbed{
+					Title:       "❌ 오류",
+					Description: fmt.Sprintf("개수의 값은 %d보다 작아야해요.", int(LIST_MAX_VALUE)),
+					Color:       utils.EmbedFail,
+				}, m.Reference())
+				return
+			}
 		}
 	case *utils.InteractionCreate:
 		m.DeferReply(true)
