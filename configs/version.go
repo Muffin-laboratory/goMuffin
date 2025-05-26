ea0dd64df8b446ab14158186605bb99c462b0d6c
Siwoo Jeon <me@migan.co.kr>
2025-05-26 22:50:18 +0900

    chore: version

--- configs/version.go
+++ configs/version.go
@@ -7,7 +7,7 @@ import (
 	"git.wh64.net/muffin/goMuffin/utils"
 )
 
-const MUFFIN_VERSION = "5.1.0-gopher_release.250524a"
+const MUFFIN_VERSION = "5.1.1-gopher_release.250526a"
 
 var updatedString string = utils.RegexpDecimals.FindAllStringSubmatch(MUFFIN_VERSION, -1)[3][0]
 
