45cd4102ed0a88f3b272210881d804a3f10999bc
Siwoo Jeon <me@migan.co.kr>
2025-08-12 22:27:15 +0900

    fix: error handling

--- handler/interaction_create.go
+++ handler/interaction_create.go
@@ -1,45 +1,28 @@
 package handler
 
 import (
-	"fmt"
 	"log"
 
 	"git.wh64.net/muffin/goMuffin/commands"
-	"git.wh64.net/muffin/goMuffin/configs"
-	"git.wh64.net/muffin/goMuffin/utils"
 	"github.com/bwmarrin/discordgo"
 )
 
 func InteractionCreate(s *discordgo.Session, i *discordgo.InteractionCreate) {
-	var err error
 	switch i.Type {
 	case discordgo.InteractionApplicationCommand:
-		err = commands.GetDiscommand().ChatInputRun(i.ApplicationCommandData().Name, s, i)
+		err := commands.GetDiscommand().ChatInputRun(i.ApplicationCommandData().Name, s, i)
 		if err != nil {
-			goto ErrMsg
+			log.Println(err)
 		}
 	case discordgo.InteractionMessageComponent:
-		err = commands.GetDiscommand().ComponentRun(s, i)
+		err := commands.GetDiscommand().ComponentRun(s, i)
 		if err != nil {
-			goto ErrMsg
+			log.Println(err)
 		}
 	case discordgo.InteractionModalSubmit:
-		err = commands.GetDiscommand().ModalRun(s, i)
+		err := commands.GetDiscommand().ModalRun(s, i)
 		if err != nil {
-			goto ErrMsg
+			log.Println(err)
 		}
 	}
-
-	// 아 몰라 goto 쓸래
-ErrMsg:
-	log.Fatalln(err)
-	owner, _ := s.User(configs.GetConfig().Bot.OwnerID)
-	utils.NewMessageSender(&utils.InteractionCreate{
-		InteractionCreate: i,
-		Session:           s,
-	}).
-		AddComponents(utils.GetErrorContainer(discordgo.TextDisplay{Content: fmt.Sprintf("오류가 발생하였어요. 만약 계속 발생한다면, %s으로 연락해주세요.", utils.InlineCode(owner.Username))})).
-		SetComponentsV2(true).
-		SetReply(true).
-		Send()
 }
