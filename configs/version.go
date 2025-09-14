bc95ce2de1c466fa6f271e3b494553ee726547dd
Siwoo Jeon <me@migan.co.kr>
2025-09-14 13:46:36 +0900

    fix: couldn't load components and modals

--- configs/version.go
+++ configs/version.go
@@ -7,7 +7,7 @@ import (
 	"git.wh64.net/muffin/goMuffin/utils"
 )
 
-const MuffinVersion = "7.0.0-madeleine_v_two_canary.250908a"
+const MuffinVersion = "7.0.0-madeleine_v_two_canary.250914a"
 
 var updatedString string = utils.RegexpDecimals.FindAllStringSubmatch(MuffinVersion, -1)[3][0]
 
--- load.go
+++ load.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"git.wh64.net/muffin/goMuffin/chatbot"
+	_ "git.wh64.net/muffin/goMuffin/components"
 	"git.wh64.net/muffin/goMuffin/configs"
 	"git.wh64.net/muffin/goMuffin/handler"
+	_ "git.wh64.net/muffin/goMuffin/modals"
 	"github.com/bwmarrin/discordgo"
 )
 
