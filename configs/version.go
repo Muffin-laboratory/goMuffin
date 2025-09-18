b20d7e6ed305a3fbee2980714dbb08c07d8b7179
Siwoo Jeon <me@migan.co.kr>
2025-09-18 20:25:14 +0900

    fix: version's branch

--- configs/version.go
+++ configs/version.go
@@ -10,7 +10,7 @@ import (
 	"git.wh64.net/muffin/goMuffin/utils"
 )
 
-var MuffinVersion = fmt.Sprintf("7.0.0-madeleine_v_two_%s.250917a", CurrentBranch)
+var MuffinVersion = fmt.Sprintf("7.0.0-madeleine_v_two_%s.250918a", CurrentBranch)
 
 var updatedString string = utils.RegexpDecimals.FindAllStringSubmatch(MuffinVersion, -1)[3][0]
 
@@ -30,7 +30,11 @@ var CurrentBranch = func() string {
 	cmd.Stdout = &out
 
 	if err := cmd.Run(); err != nil {
-		return "main"
+		return "release"
+	}
+
+	if strings.Contains(out.String(), "main") {
+		return "release"
 	}
 
 	return strings.Trim(out.String(), "\n")
