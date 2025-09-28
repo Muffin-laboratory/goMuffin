541d3a8ca25e5d2ad788fcd6bc8805302d865e35
Siwoo Jeon <me@migan.co.kr>
2025-09-28 22:13:29 +0900

    fix: user can mention another user

--- configs/version.go
+++ configs/version.go
@@ -10,7 +10,7 @@ import (
 	"git.wh64.net/muffin/goMuffin/utils"
 )
 
-var MuffinVersion = fmt.Sprintf("7.0.0-madeleine_v_two_%s.250928a", CurrentBranch)
+var MuffinVersion = fmt.Sprintf("7.0.0-madeleine_v_two_%s.250928b", CurrentBranch)
 
 var updatedString string = utils.RegexpDecimals.FindAllStringSubmatch(MuffinVersion, -1)[3][0]
 
--- handler/message_create.go
+++ handler/message_create.go
@@ -64,6 +64,12 @@ func MessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		utils.NewMessageSender(m).
 			SetContent(result).
 			SetReply(true).
+			SetAllowedMentions(discordgo.MessageAllowedMentions{
+				Parse:       []discordgo.AllowedMentionType{},
+				Users:       []string{},
+				Roles:       []string{},
+				RepliedUser: true,
+			}).
 			Send()
 
 		return
