a4ce8b172ba8cb3a2d11ece163c9f764f064b9dd
Siwoo Jeon <me@migan.co.kr>
2025-10-03 21:36:16 +0900

    refactor: ComponentBuilder interface

--- builders/builder.go
+++ builders/builder.go
@@ -2,11 +2,6 @@ package builders
 
 import "github.com/bwmarrin/discordgo"
 
-type Builder[T any] interface {
-	Build() discordgo.MessageComponent
-}
-
 type ComponentBuilder[T any] interface {
-	Builder[T]
-	AddComponents(components ...discordgo.MessageComponent) T
+	Build() discordgo.MessageComponent
 }
