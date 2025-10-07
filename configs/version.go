330e0490d7206751c7bfdd0f213a8da2bdda86e5
Siwoo Jeon <me@migan.co.kr>
2025-10-07 14:14:54 +0900

    fix: workflow file

--- configs/version.go
+++ configs/version.go
@@ -10,7 +10,7 @@ import (
 	"git.wh64.net/muffin/goMuffin/utils"
 )
 
-var MuffinVersion = fmt.Sprintf("7.0.0-madeleine_v_two_%s.251006a", CurrentBranch)
+var MuffinVersion = fmt.Sprintf("7.0.0-madeleine_v_two_%s.251007a", CurrentBranch)
 
 var updatedString string = utils.RegexpDecimals.FindAllStringSubmatch(MuffinVersion, -1)[3][0]
 
