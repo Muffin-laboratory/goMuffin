765165aae6b522c77063cedb4463684b9b545453
Siwoo Jeon <me@migan.co.kr>
2025-10-19 15:12:39 +0900

    feat: add 12hours and reply user in information user command

--- commands/subcommands/information/user.go
+++ commands/subcommands/information/user.go
@@ -11,6 +11,14 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
+func boolToString(k bool) string {
+	if k {
+		return "활성화"
+	}
+
+	return "비활성화"
+}
+
 func InfoUser(i *builders.InteractionCreate) error {
 	accCreatedTimestamp, err := discordgo.SnowflakeTimestamp(i.User.ID)
 	if err != nil {
@@ -44,26 +52,28 @@ func InfoUser(i *builders.InteractionCreate) error {
 						AddText(fmt.Sprintf("### %s님의 정보", i.User.GlobalName)).
 						AddText(fmt.Sprintf("- **디스코드 가입일**\n> %s", utils.Time(&accCreatedTimestamp, utils.RelativeTime))).
 						AddText(fmt.Sprintf("- **머핀봇 가입일**\n> %s", utils.Time(&dbUser.CreatedAt, utils.RelativeTime))),
+					builders.TextDisplayBuilder(fmt.Sprintf("- **현재 모드**\n> `%s`", dbUser.ModeString())),
+					builders.TextDisplayBuilder(fmt.Sprintf("- **답장 멘션 사용 여부**\n> `%s`", boolToString(dbUser.ReplyUser))),
+					builders.TextDisplayBuilder(fmt.Sprintf("- **마지막 채팅 이후 12 시간이 지났을 때 새로운 채팅 생성 여부**\n> `%s`", boolToString(dbUser.CreateNewChatAfter12Hours))),
+					builders.TextDisplayBuilder(fmt.Sprintf("- **현재 채팅**\n> %s", currentChat.Name)),
+					builders.TextDisplayBuilder(fmt.Sprintf("- **총 채팅량**\n> `%d`개", chatLength)),
+					builders.ActionsRowBuilder(
+						builders.ButtonBuilder().
+							SetStyle(discordgo.LinkButton).
+							SetLabel("개인정보처리방침").
+							SetURL(configs.GetConfig().Service.PrivacyPolicyURL).
+							SetEmoji(discordgo.ComponentEmoji{Name: "🔗"}),
+						builders.ButtonBuilder().
+							SetStyle(discordgo.LinkButton).
+							SetLabel("서비스 이용약관").
+							SetURL(configs.GetConfig().Service.TermOfServiceURL).
+							SetEmoji(discordgo.ComponentEmoji{Name: "🔗"}),
+						builders.ButtonBuilder().
+							SetStyle(discordgo.DangerButton).
+							SetLabel("탈퇴").
+							SetCustomID(utils.MakeUserInformationDeregister(i.User.ID)),
+					),
 				),
-			builders.TextDisplayBuilder(fmt.Sprintf("- **현재 모드**\n> %s", dbUser.ModeString())),
-			builders.TextDisplayBuilder(fmt.Sprintf("- **현재 채팅**\n> %s", currentChat.Name)),
-			builders.TextDisplayBuilder(fmt.Sprintf("- **총 채팅량**\n> `%d`개", chatLength)),
-			builders.ActionsRowBuilder(
-				builders.ButtonBuilder().
-					SetStyle(discordgo.LinkButton).
-					SetLabel("개인정보처리방침").
-					SetURL(configs.GetConfig().Service.PrivacyPolicyURL).
-					SetEmoji(discordgo.ComponentEmoji{Name: "🔗"}),
-				builders.ButtonBuilder().
-					SetStyle(discordgo.LinkButton).
-					SetLabel("서비스 이용약관").
-					SetURL(configs.GetConfig().Service.TermOfServiceURL).
-					SetEmoji(discordgo.ComponentEmoji{Name: "🔗"}),
-				builders.ButtonBuilder().
-					SetStyle(discordgo.DangerButton).
-					SetLabel("탈퇴").
-					SetCustomID(utils.MakeUserInformationDeregister(i.User.ID)),
-			),
 		).
 		SetComponentsV2(true).
 		SetEphemeral(true).
--- configs/version.go
+++ configs/version.go
@@ -10,7 +10,7 @@ import (
 	"git.wh64.net/muffin/goMuffin/utils"
 )
 
-var MuffinVersion = fmt.Sprintf("7.0.0-pretzel_%s.251019a", CurrentBranch)
+var MuffinVersion = fmt.Sprintf("7.0.0-pretzel_%s.251019b", CurrentBranch)
 
 var updatedString string = utils.RegexpDecimals.FindAllStringSubmatch(MuffinVersion, -1)[3][0]
 
