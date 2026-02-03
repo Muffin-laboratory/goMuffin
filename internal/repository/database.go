4a58a6c0d89e6a539ac960e0e95af03c2376c75d
Siwoo Jeon <me@migan.co.kr>
2026-02-03 20:34:04 +0900

    feat: use sync.Once in database, github client

--- internal/repository/database.go
+++ internal/repository/database.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"context"
 	"log"
+	"sync"
 	"time"
 
 	"github.com/Muffin-laboratory/goMuffin/internal/cache"
@@ -23,24 +24,24 @@ type MuffinDatabase struct {
 const timeToExpire = time.Hour * 12
 
 var instance *MuffinDatabase
-
-func init() {
-	client, err := mongo.Connect(options.Client().ApplyURI(configs.GetConfig().Database.URL))
-	if err != nil {
-		log.Panicln(err)
-	}
-
-	instance = &MuffinDatabase{
-		Client:    client,
-		Knowledge: &KnowledgeCollection{client.Database(configs.GetConfig().Database.Name).Collection("learn"), cache.New[string, *knowledgeCacheItem](timeToExpire)},
-		Texts:     client.Database(configs.GetConfig().Database.Name).Collection("text"),
-		Memory:    &MemoryCollection{client.Database(configs.GetConfig().Database.Name).Collection("memory"), cache.New[string, *memoryCacheItem](timeToExpire)},
-		Users:     &UserCollection{client.Database(configs.GetConfig().Database.Name).Collection("user"), cache.New[string, User](timeToExpire)},
-		Chats:     newChatCollection(client.Database(configs.GetConfig().Database.Name).Collection("chat")),
-	}
-}
+var once sync.Once
 
 func GetDatabase() *MuffinDatabase {
+	once.Do(func() {
+		client, err := mongo.Connect(options.Client().ApplyURI(configs.GetConfig().Database.URL))
+		if err != nil {
+			log.Panicln(err)
+		}
+
+		instance = &MuffinDatabase{
+			Client:    client,
+			Knowledge: &KnowledgeCollection{client.Database(configs.GetConfig().Database.Name).Collection("learn"), cache.New[string, *knowledgeCacheItem](timeToExpire)},
+			Texts:     client.Database(configs.GetConfig().Database.Name).Collection("text"),
+			Memory:    &MemoryCollection{client.Database(configs.GetConfig().Database.Name).Collection("memory"), cache.New[string, *memoryCacheItem](timeToExpire)},
+			Users:     &UserCollection{client.Database(configs.GetConfig().Database.Name).Collection("user"), cache.New[string, User](timeToExpire)},
+			Chats:     newChatCollection(client.Database(configs.GetConfig().Database.Name).Collection("chat")),
+		}
+	})
 	return instance
 }
 
--- internal/repository/github.go
+++ internal/repository/github.go
@@ -1,15 +1,18 @@
 package repository
 
 import (
+	"sync"
+
 	"github.com/google/go-github/v74/github"
 )
 
 var ghInstance *github.Client
+var ghOnce sync.Once
 
 func GetGHClient() *github.Client {
-	if ghInstance == nil {
+	ghOnce.Do(func() {
 		ghInstance = github.NewClient(nil)
-	}
+	})
 
 	return ghInstance
 }
