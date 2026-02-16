7219c3e06b9af651a6e6648c260ad820f4776e7a
Siwoo Jeon <me@migan.co.kr>
2026-02-16 16:08:19 +0900

    refactor: remove unused code

--- internal/bot/builders/time.go
+++ internal/bot/builders/time.go
@@ -18,17 +18,6 @@ const (
 	RelativeTime = "R"
 )
 
-func InlineCode(content string) string {
-	return fmt.Sprintf("`%s`", content)
-}
-
-func CodeBlock(language string, content string) string {
-	if content == "" {
-		return fmt.Sprintf("```\n%s\n```", language)
-	}
-	return fmt.Sprintf("```%s\n%s\n```", language, content)
-}
-
 func Time(time *time.Time, style string) string {
 	if style == "" {
 		return fmt.Sprintf("<t:%d>", time.Unix())
