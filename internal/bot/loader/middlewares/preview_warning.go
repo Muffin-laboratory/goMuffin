cbed0078a439eca5f4c42a7c119ac85097ae2e25
Siwoo Jeon <me@migan.co.kr>
2026-02-22 16:03:08 +0900

    fix: preview warning

--- internal/bot/loader/middlewares/preview_warning.go
+++ internal/bot/loader/middlewares/preview_warning.go
@@ -11,18 +11,22 @@ func ShowPreviewWarningMessage() handler.Middleware {
 		return func(e *handler.InteractionEvent) error {
 			next(e)
 
-			_, err := e.CreateFollowupMessage(
-				discord.NewMessageCreateV2(
-					discord.NewContainer(
-						discord.NewTextDisplay("### ⚠️ 경고"),
-						discord.NewTextDisplay("- 해당 버전의 머핀봇은 아직 개발단계로, **매우 불안정한** 상태에요. "+
-							"따라서 심각한 오류가 발생할 수 있어요."),
-						discord.NewTextDisplayf("-# 현재 버전: %s", configs.MuffinVersion),
-					),
-				).
-					WithEphemeral(true),
-			)
-			return err
+			if e.Type() == discord.InteractionTypeApplicationCommand {
+				_, err := e.CreateFollowupMessage(
+					discord.NewMessageCreateV2(
+						discord.NewContainer(
+							discord.NewTextDisplay("### ⚠️ 경고"),
+							discord.NewTextDisplay("- 해당 버전의 머핀봇은 아직 개발단계로, **매우 불안정한** 상태에요. "+
+								"따라서 심각한 오류가 발생할 수 있어요."),
+							discord.NewTextDisplayf("-# 현재 버전: %s", configs.MuffinVersion),
+						),
+					).
+						WithEphemeral(true),
+				)
+				return err
+			}
+
+			return nil
 		}
 	}
 }
